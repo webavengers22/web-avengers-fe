17360a63d16f38dd21776b208b511465d570f14b
Juwon <wndnjs2037@naver.com>
2023-05-09 00:17:04 +0900

    파일구조 초기 생성

--- web-averngers-fe/src/App.tsx
+++ web-averngers-fe/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React, 웹벤져스 테스트 페이지 입니다.</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
